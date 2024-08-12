4d6e891fcfc8cecfdc6af21dea11bd77a593956c
Prakhar <prakhargenuine9336@gmail.com>
2024-08-12 19:54:24 +0530

    Hero,Highlight,Navbar-->Completed

---
+++ 1APPLE/src/components/VideoCarousel.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from "react";
+import { hightlightsSlides } from "../constants";
+
+const VideoCarousel = () => {
+
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+
+    const [video, setVideo] = useState({
+        isEnd: false,
+        startPlay: false,
+        videoId: 0,
+        isLastVideo: false,
+        isPlaying: false,
+    })
+
+    const [loadedData, setLoadedData] = useState([]);
+
+    const  {isEnd , startPlay , isLastVideo , videoId , isPlaying} = video;
+
+    useEffect(()=>{
+        if(loadedData.length > 3){
+            if(!isPlaying){
+                videoRef.current[videoId].pause();
+            }else{
+                startPlay && videoRef.current[videoId].play();
+            }
+        }
+    }, [startPlay, videoId, isPlaying, loadedData])
+
+    useEffect(() =>{
+        const currentProgress = 0;
+        let span = videoSpanRef.current;
+
+        if(span[videoId]){
+            //animate the progress of the video
+            let anim = gsap.to(span[videoId], {
+                //what happens when animation is updated
+                onUpdate: () =>{
+
+                },
+
+                //what happens when animation is completed
+                onComplete: ()=>{
+
+                }
+            })
+        }
+    }, [videoId,startPlay])
+
+    return (
+        <>
+            <div className="flex items-center">
+                {hightlightsSlides.map((list , i) => (
+                    //we are not using braces here in this function because it is an 'immediate return' function.
+                    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                        <div className="video-carousel_container">
+                            <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                                <video id="video" placeInline={true} preload="auto" muted ref={(el) => (videoRef.current[i] = el)} onPlay={(prevVideo)=>({
+                                    ...prevVideo, isPlaying: true
+                                })}>
+                                    <source src={list.video} type="video/mp4"/>
+                                </video>
+                            </div>
+                            <div className="absolute top-12 left-[5%] z-10">
+                                {list.textLists.map((text) =>(
+                                    <p key={text} className="md:text-2xl text-xl font-medium">
+                                        {text}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default VideoCarousel
