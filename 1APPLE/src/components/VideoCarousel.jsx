2742ead57532ba05c2c0d64ef87c158ceb42ba27
Prakhar <prakhargenuine9336@gmail.com>
2024-08-13 12:50:52 +0530

    Updating my project

--- 1APPLE/src/components/VideoCarousel.jsx
+++ 1APPLE/src/components/VideoCarousel.jsx
@@ -2,80 +2,99 @@ import { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
 
-    const videoRef = useRef([]);
-    const videoSpanRef = useRef([]);
-    const videoDivRef = useRef([]);
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
 
-    const [video, setVideo] = useState({
-        isEnd: false,
-        startPlay: false,
-        videoId: 0,
-        isLastVideo: false,
-        isPlaying: false,
-    })
+  const [loadedData, setLoadedData] = useState([]);
 
-    const [loadedData, setLoadedData] = useState([]);
+  const { isEnd, startPlay, isLastVideo, videoId, isPlaying } = video;
 
-    const  {isEnd , startPlay , isLastVideo , videoId , isPlaying} = video;
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
 
-    useEffect(()=>{
-        if(loadedData.length > 3){
-            if(!isPlaying){
-                videoRef.current[videoId].pause();
-            }else{
-                startPlay && videoRef.current[videoId].play();
-            }
-        }
-    }, [startPlay, videoId, isPlaying, loadedData])
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
 
-    useEffect(() =>{
-        const currentProgress = 0;
-        let span = videoSpanRef.current;
+    if (span[videoId]) {
+      //animate the progress of the video
+      let anim = gsap.to(span[videoId], {
+        //what happens when animation is updated
+        onUpdate: () => {},
 
-        if(span[videoId]){
-            //animate the progress of the video
-            let anim = gsap.to(span[videoId], {
-                //what happens when animation is updated
-                onUpdate: () =>{
+        //what happens when animation is completed
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
 
-                },
-
-                //what happens when animation is completed
-                onComplete: ()=>{
-
-                }
-            })
-        }
-    }, [videoId,startPlay])
-
-    return (
-        <>
-            <div className="flex items-center">
-                {hightlightsSlides.map((list , i) => (
-                    //we are not using braces here in this function because it is an 'immediate return' function.
-                    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
-                        <div className="video-carousel_container">
-                            <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                                <video id="video" placeInline={true} preload="auto" muted ref={(el) => (videoRef.current[i] = el)} onPlay={(prevVideo)=>({
-                                    ...prevVideo, isPlaying: true
-                                })}>
-                                    <source src={list.video} type="video/mp4"/>
-                                </video>
-                            </div>
-                            <div className="absolute top-12 left-[5%] z-10">
-                                {list.textLists.map((text) =>(
-                                    <p key={text} className="md:text-2xl text-xl font-medium">
-                                        {text}
-                                    </p>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          //we are not using braces here in this function because it is an 'immediate return' function.
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video
+                  id="video"
+                  placeInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={(prevVideo) => ({
+                    ...prevVideo,
+                    isPlaying: true,
+                  })}
+                >
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
                 ))}
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
+        ))}
+      </div>
 
-export default VideoCarousel
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              ref={(el) => (videoDivRef.current[i] = el)}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+            >
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(el) => (videoSpanRef.current[i] = el)} />
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
