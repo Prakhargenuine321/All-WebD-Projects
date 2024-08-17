66636396910b20e282975c4472e790d4437c373f
Prakhar <prakhargenuine9336@gmail.com>
2024-08-18 00:21:06 +0530

    90% completed

--- 2Level1/script.js
+++ 2Level1/script.js
@@ -1,64 +1,120 @@
+var timeout;
+
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-function circleMouseFollower(){
-    window.addEventListener("mousemove", function(details){
-        document.querySelector("#mini-circle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "20",
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 1,
+  })
+    .to(".bounding-elem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 1,
+      stagger: 0.2,
+      delay: -1,
     })
-}
-
-circleMouseFollower();
-
-function circleChaptaKaro(){
-    let xscale = 1;
-    let yscale = 1;
-
-    let xprev = 0;
-    let yprev = 0;
-    window.addEventListener("mousemove", function(details){
-        details.clientX - xprev;
-        xprev = details.clientX;
+    .to(".bounding-elem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 1,
+      stagger: 0.2,
     });
+
+  gsap.from("#mini-heading h5", {
+    opacity: 0,
+    y: 20,
+    stagger: 0.2,
+    delay: 0.2,
+    duration: 0.6,
+  });
+
+  gsap.from("#hero-footer", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+  });
 }
 
-function firstPageAnim(){
-    var tl = gsap.timeline();
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
 
-    tl.from("#nav", {
-        y: '20',
-        opacity: 0,
-        ease: Expo.easeInOut,
-        duration:1
-    })
-    .to(".bounding-elem", {
-        y: 0,
-        ease: Expo.easeInOut,
-        duration:1,
-        stagger:0.2,
-        delay:-1
-    })
-    .to(".bounding-elem", {
-        y: 0,
-        ease: Expo.easeInOut,
-        duration:1,
-        stagger:0.2
-    })
+  var xprev = 0;
+  var yprev = 0;
 
-    gsap.from("#mini-heading h5", {
-        opacity: 0,
-        y: 20,
-        stagger: 0.2,
-        delay:0.2,
-        duration:0.6
-    })
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
 
-    gsap.from("#hero-footer", {
-        opacity: 0,
-        duration:1,
-        delay:1
-    })
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#mini-circle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 0.5);
+  });
 }
 
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#mini-circle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleChaptaKaro();
+circleMouseFollower();
 firstPageAnim();
+
+
+//animating "#second"...
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+      gsap.to(elem.querySelector("h1"), {
+        opacity: .7,
+        x: 0
+      })
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      let diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+
+      gsap.to(elem.querySelector("h1"), {
+        opacity: .3,
+        x: 30
+      })
+    });
+  });
