67b612d089ea28f4ee9eca0ea945814caa1981e5
Prakhar <prakhargenuine9336@gmail.com>
2024-08-26 23:08:57 +0530

    Style update with template

---
+++ 03eComProject/homeProductCards.js
@@ -0,0 +1,14 @@
+const ProductContainer = document.querySelector('#productContainer');
+
+const productTemplate = document.querySelector('#productTemplate');
+
+
+export const showProductContainer = (products) =>{
+    if(!products){
+        return false;
+    }
+
+    products.forEach((curElem) => {
+        
+    })
+}
--- 03eComProject/main.js
+++ 03eComProject/main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import './style.css';
+import products from "./api/products.json";
+import { showProductContainer } from './homeProductCards';
+
+// Define a function named `showProductContainer` that takes an array of products as input.
+
+showProductContainer(products);
