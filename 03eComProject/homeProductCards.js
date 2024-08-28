e5de04c91115ceca34d5d7364439b5868201bfb8
Prakhar <prakhargenuine9336@gmail.com>
2024-08-28 22:54:19 +0530

    Button functionality updated

--- 03eComProject/homeProductCards.js
+++ 03eComProject/homeProductCards.js
@@ -1,4 +1,6 @@
-const ProductContainer = document.querySelector('#productContainer');
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
+const productContainer = document.querySelector('#productContainer');
 
 const productTemplate = document.querySelector('#productTemplate');
 
@@ -8,7 +10,28 @@ export const showProductContainer = (products) =>{
         return false;
     }
 
-    products.forEach((curElem) => {
-        
+    products.forEach((curProd) => {
+        //destructuring the data
+        const{brand , category , description , id , image , name , price , stock} = curProd;
+
+        //to use template we need to clone it
+        const productClone = document.importNode(productTemplate.content , true);
+
+        productClone.querySelector("#cardValue").setAttribute('id' , `card${id}`);
+
+        productClone.querySelector(".productName").textContent = name;
+        productClone.querySelector(".productImage").src = image;
+        productClone.querySelector(".productImage").alt = name;
+        productClone.querySelector(".productStock").textContent = stock;
+        productClone.querySelector(".productDescription").textContent = description;
+        productClone.querySelector(".category").textContent = category;
+        productClone.querySelector(".productPrice").textContent = `₹${price}`;
+        productClone.querySelector(".productActualPrice").textContent = `₹${price*2}`;
+
+        productClone.querySelector(".stockElement").addEventListener("click", (event)=>{
+            homeQuantityToggle(event, id, stock);
+        })
+
+        productContainer.append(productClone);
     })
 }
---
+++ 03eComProject/homeQuantityToggle.js
@@ -0,0 +1,4 @@
+export const homeQuantityToggle = (event, id, stock) => {
+    const currentCardElement = document.querySelector(`#card${id}`);
+    console.log(currentCardElement);
+};
